31f35e0f5d831f11630a66fd4b646f2693369123
IAmBod <boodeikaroly@gmail.com>
2026-05-28 21:54:57 +0200

    feat(coord): Coordinator interface and noop implementation

---
+++ internal/coord/noop/noop_test.go
@@ -0,0 +1,27 @@
+package noop
+
+import (
+	"context"
+	"testing"
+)
+
+func TestNoopAlwaysAcquires(t *testing.T) {
+	t.Parallel()
+	c := New()
+	release, acquired, err := c.TryAcquire(context.Background(), "https://e/feed-1")
+	if err != nil {
+		t.Fatalf("unexpected err: %v", err)
+	}
+	if !acquired {
+		t.Fatal("expected acquired=true")
+	}
+	if release == nil {
+		t.Fatal("expected non-nil release")
+	}
+	if err := release(context.Background()); err != nil {
+		t.Fatalf("release err: %v", err)
+	}
+	if err := c.Close(); err != nil {
+		t.Fatalf("close err: %v", err)
+	}
+}
